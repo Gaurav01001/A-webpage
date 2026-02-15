9b53cd630acafc86dc7e0e0785286236332c9c94
Gaurav Saha <gauravsaha024@gmail.com>
2026-02-15 13:09:46 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Hero from './Components/Hero'
 import About from './Components/About'
-import Navebar from './Components/Navebar'
+import Navbar from './Components/Navbar'
+import Features from './Components/Features'
+import Story from './Components/Story'
+
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
+
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-hidden'>
+      <Navbar />
       <Hero />
-      <Navebar />
       <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { ScrollTrigger } from 'gsap/all'
 gsap.registerPlugin(ScrollTrigger);
 import Animated from './Animated'
-import Navebar from './Navebar'
+
 const About = () => {
     useGSAP(() => {
         const clipanimation = gsap.timeline({
@@ -18,37 +18,33 @@ const About = () => {
             }
         })
         clipanimation.to('.mask-clip-path', {
-            width: '100vh',
+            width: '100vw',
             height: '100vh',
             borderRadius: '0',
         })
     })
     return (
-        <>
-            <Navebar />
-            <div id='about' className='min-h-screen w-screen '>
-                <div className='relative mb-8 mt-36 flex flex-col items-center gap-5' >
-                    <h2 className='font-general text-sm uppercase md:text-[10px]'>Ggs Hope Yo doing good </h2>
+        <div id='about' className='min-h-screen w-screen '>
+            <div className='relative mb-8 mt-36 flex flex-col items-center gap-5' >
+                <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Midah</h2>
 
-                    <Animated
-                        title="someday i h<b>o</b>pe to  summit a <b>p</b>eak" container="mt-5 !text-black text-center" />
+                <Animated
+                    title="someday i h<b>o</b>pe to  summit a <b>p</b>eak" container="mt-5 !text-black text-center" />
 
-
-
-                    <div className="about-subtext">
-                        <p>Genz in general are cooked</p>
-                    </div>
-                </div>
-                <div className='h-dvh w-screen ' id='clip'>
-                    <div className="mask-clip-path about-image">
-                        <img src="img/about.webp"
-                            alt='Background'
-                            className='absolute left-0 top-0 size-full object-cover' />
-
-                    </div>
+                <div className="about-subtext">
+                    <p>The Game of Games begins-your life, now an epic MMORPG</p>
+                    <p>Zentry unites every player from countless games and platforms</p>
                 </div>
             </div>
-        </>
+            <div className='h-dvh w-screen ' id='clip'>
+                <div className="mask-clip-path about-image">
+                    <img src="img/about.webp"
+                        alt='Background'
+                        className='absolute left-0 top-0 size-full object-cover' />
+
+                </div>
+            </div>
+        </div>
     )
 }
 
